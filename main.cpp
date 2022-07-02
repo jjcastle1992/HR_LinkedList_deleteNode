61999ee65eb8bfc0a578755487ded0c21c19aae5
James Castle <james.castle@g.austincc.edu>
2022-07-02 06:17:53 -0500

    deleteNode: fix bug in code where tempNode was not set to nullptr. Now maintains continuity of list.

--- main.cpp
+++ main.cpp
@@ -90,7 +90,8 @@ SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* &head, int targetPosition
                 //Grab next node's next and set to current node's next.
                 head->next = head->next->next;
                 //delete next node.
-                tempNode->next = head;
+                //tempNode->next = head;
+                tempNode = nullptr;
                 delete tempNode;
             }
             else {
